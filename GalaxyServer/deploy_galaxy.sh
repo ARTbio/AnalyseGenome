994b300370f81b89ab0149eabf6660f23fb4af62
Christophe Antoniewski <drosofff@users.noreply.github.com>
2022-11-10 13:59:53 +0100

    Update deploy_galaxy.sh

--- GalaxyServer/deploy_galaxy.sh
+++ GalaxyServer/deploy_galaxy.sh
@@ -2,7 +2,7 @@ git clone https://github.com/ARTbio/AnalyseGenome.git
 git clone https://github.com/galaxyproject/galaxy.git -b release_22.05
 cp ~/galaxy/config/galaxy.yml.sample ~/galaxy/config/galaxy.yml
 sed -i.bak "s/# bind: localhost:8080/bind: 0.0.0.0:80/" ~/galaxy/config/galaxy.yml
-sed -e.bak "s/#admin_users: null/admin_users: admin@galaxy.org/" ~/galaxy/config/galaxy.yml
+sed -i.bak "s/#admin_users: null/admin_users: admin@galaxy.org/" ~/galaxy/config/galaxy.yml
 rm -rf ~/galaxy/client ~/galaxy/static
 cd ~/galaxy && wget https://psilo.sorbonne-universite.fr/index.php/s/Kw9y8LgYyXBq3pr/download/client.tar.gz \
                     https://psilo.sorbonne-universite.fr/index.php/s/YCEe2XNbgLMf2Px/download/static.tar.gz
