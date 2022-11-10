e5662c227de77f9919ee7604623b61600e4b03e8
Christophe Antoniewski <drosofff@users.noreply.github.com>
2022-11-10 13:15:03 +0100

    Create deploy_galaxy.sh

---
+++ GalaxyServer/deploy_galaxy.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/ARTbio/AnalyseGenome.git
+git clone https://github.com/galaxyproject/galaxy.git -b release_22.05
+cp ~/galaxy/config/galaxy.yml.sample ~/galaxy/config/galaxy.yml
+sed -i.bak "s/# bind: localhost:8080/bind: 0.0.0.0:80/" ~/galaxy/config/galaxy.yml
+sed -e.bak "s/#admin_users: null/admin_users: admin@galaxy.org/" ~/galaxy/config/galaxy.yml
+rm -rf ~/galaxy/client ~/galaxy/static
+cd ~/galaxy && wget https://psilo.sorbonne-universite.fr/index.php/s/Kw9y8LgYyXBq3pr/download/client.tar.gz \
+                    https://psilo.sorbonne-universite.fr/index.php/s/YCEe2XNbgLMf2Px/download/static.tar.gz
+tar -xvf static.tar.gz && tar -xvf client.tar.gz
+cp ~/AnalyseGenome/GalaxyServer/job_conf.xml ~/galaxy/config/
+pwd
+screen -dmS galaxyscreen sh run.sh # this command runs in the screen session galaxyscreen until killed
