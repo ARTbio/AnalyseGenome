852d5ee36350545f934c3b26e8adc24ebec34781
Christophe Antoniewski <drosofff@users.noreply.github.com>
2022-11-11 14:05:50 +0100

    Create CLI_parsing.sh

---
+++ Exercises/CLI_parsing.sh
@@ -0,0 +1,11 @@
+grep -P "(^ID)|(^ +[GATCN ]+\d+)" transposon_sequence_set_v9.5.embl.txt > transposon_sequence_set_v9.5.fa
+
+sed -i.bak -E "s/^ID   />/" transposon_sequence_set_v9.5.fa
+
+sed -i.bak2 -E "s/(>[^ ]+) .+/\1/g" transposon_sequence_set_v9.5.fa
+
+sed -i.bak3 -E "s/([GATCN]+) /\1/g" transposon_sequence_set_v9.5.fa
+
+sed -i.bak4 -r "s/^ +//g" transposon_sequence_set_v9.5.fa
+
+sed -i.bak5 -r "s/ +[0-9]+//g" transposon_sequence_set_v9.5.fa
