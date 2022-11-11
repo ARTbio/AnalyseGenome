de63936cd91ca63162f8031055a5cec67434c7a8
Christophe Antoniewski <drosofff@users.noreply.github.com>
2022-11-11 19:51:15 +0100

    Update install_galaxy_tools.sh

--- GalaxyServer/install_galaxy_tools.sh
+++ GalaxyServer/install_galaxy_tools.sh
@@ -8,6 +8,7 @@ screen -dmS ephemeris /root/galaxy/.venv/bin/shed-tools install \
                 -g http://127.0.0.1 \
                 -a "$API" \
                 -u admin@galaxy.org \
-                -t ~/AnalyseGenome/GalaxyServer/bg_tools.yml
+                -t ~/AnalyseGenome/GalaxyServer/bg_tools.yml \
+                > tools.log
 echo "tool installation is complete, press any key to terminate this screen session\n"
 read interrupt
