95b03fbf7cb03bd4db2058dbe6a23671310d33ef
Christophe Antoniewski <drosofff@users.noreply.github.com>
2022-11-11 14:01:06 +0100

    Update install_galaxy_tools.sh

--- GalaxyServer/install_galaxy_tools.sh
+++ GalaxyServer/install_galaxy_tools.sh
@@ -1,8 +1,10 @@
+echo "please enter your admin API key: "
+read API
 /root/galaxy/.venv/bin/pip install ephemeris
 echo "Installation to galaxy tools may take ~1 hour. Keep cool\n"
 echo "to follow installation, please use 'screen -r ephemeris' command\n"
 screen -dmS ephemeris /root/galaxy/.venv/bin/shed-tools install \
                 -g http://127.0.0.1 \
-                -a "$1" \
+                -a "$API" \
                 -u admin@galaxy.org \
                 -t ~/AnalyseGenome/GalaxyServer/bg_tools.yml
