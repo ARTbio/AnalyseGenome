1a0457f57b21e999a9d4fddf12a4cfbbcb712687
Christophe Antoniewski <drosofff@users.noreply.github.com>
2022-11-15 18:13:48 +0100

    Update install_galaxy_tools.sh

--- GalaxyServer/install_galaxy_tools.sh
+++ GalaxyServer/install_galaxy_tools.sh
@@ -9,5 +9,5 @@ screen -dmS ephemeris /root/galaxy/.venv/bin/shed-tools install \
                                                    -t ~/AnalyseGenome/GalaxyServer/bg_tools.yml \
                                                    --log_file ~/tools.log
 echo "To follow installation, you may use the 'screen -r ephemeris' command\n"
-echo "Alternatively, you can use the 'tail -f /root/tools.log' command and terminate by Ctrl-C"
+echo "Alternatively, you can use the 'tail -f /root/tools.log' command and terminate by Ctrl-C\n"
 echo "Installation of the 21 galaxy tools may take a long time. Keep cool\n"
