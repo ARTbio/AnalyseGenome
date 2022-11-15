aef43a1f0f936e43b9cf3c7e78e8d930921d1609
Christophe Antoniewski <drosofff@users.noreply.github.com>
2022-11-15 18:09:00 +0100

    Update install_galaxy_tools.sh

--- GalaxyServer/install_galaxy_tools.sh
+++ GalaxyServer/install_galaxy_tools.sh
@@ -2,13 +2,12 @@ echo "please enter your admin API key: "
 read API
 /root/galaxy/database/dependencies/_conda/bin/conda update conda -y
 /root/galaxy/.venv/bin/pip install ephemeris
-echo "Installation of the 21 galaxy tools may take a long time. Keep cool\n"
-echo "to follow installation, you may use 'screen -r ephemeris' command\n"
 screen -dmS ephemeris /root/galaxy/.venv/bin/shed-tools install \
                                                    -g http://127.0.0.1 \
                                                    -a "$API" \
                                                    -u admin@galaxy.org \
                                                    -t ~/AnalyseGenome/GalaxyServer/bg_tools.yml \
-                                                   --log_file ~/tools.log && \
-                                               echo "tool installation is complete, press 'enter' key to terminate this screen session.\n" && \
-                                               read interrupt
+                                                   --log_file ~/tools.log
+echo "To follow installation, you may use the 'screen -r ephemeris' command\n"
+echo "Alternatively, you can use the 'tail -f /root/tools.log' command and terminate by Ctrl-C"
+echo "Installation of the 21 galaxy tools may take a long time. Keep cool\n"
