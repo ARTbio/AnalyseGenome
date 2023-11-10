c6d7540c9250cd217253cd98b3715e4699f02917
drosofff <drosofff@gmail.com>
2023-11-10 17:36:42 +0100

    Update deploy_galaxy.sh

--- GalaxyServer/deploy_galaxy.sh
+++ GalaxyServer/deploy_galaxy.sh
@@ -2,7 +2,6 @@
 set -e
 apt update
 apt install -y python3.8-venv
-apt install git screen -y
 git clone https://github.com/ARTbio/AnalyseGenome.git
 git clone https://github.com/galaxyproject/galaxy.git -b release_23.01
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFdDo/U2shXOKSwSz1JhYY7jDCwuAt4sgPH2pw0r1fKmxJpsY7vZgwkn1XvZPDPPo9Go2LgNVCRBgRZ60f1ivlviy5D1deo/5XtMshDjJDSBaGx4QQhtjAsCgvZ/2Sx+wP2l7IVEYKzWdKens4JVZ+gIh/PwXS0PzNeWOLiCynlLncEm0nuV9Y6wCbhnUt9Zjbe/y0/Cm6wPpOqu9J2BKVerGZtnfvM2+MD6S40n4CH7iFRrnqlOlc6junnuJ6g/u2sIcYXhmD6bOttI5cOINGcDacitp7enUaiSt5ViVz0vjkHfuxr42rtq5dLMI6hQxnq8sjpfb1ygpe7HHHnHCn chris@lbcd-17.snv.jussieu.fr" >> /root/.ssh/authorized_keys
@@ -15,4 +14,4 @@ sed -i.bak "s/# virtualenv:/virtualenv: /root/galaxy/.venv" ~/galaxy/config/gala
 cp ~/AnalyseGenome/GalaxyServer/job_conf.xml ~/galaxy/config/
 sh run.sh --daemon # Triggers deploiement and leave galaxy running as a deamon
 echo "source /root/galaxy/.venv/bin/activate" >> /root/.bashrc # automatically activate galaxy .venv
-echo "Galaxy is now running as a daemon in the background\nand is controlled by systemctl"
+echo "Galaxy is now running as a daemon in the background\nand is controlled by systemctl\n"
