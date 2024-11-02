0ae82be26471af26eca20f7e83bf8fdc43a9988d
drosofff <drosofff@gmail.com>
2024-11-02 23:36:57 +0100

    Update install_galaxy_tools.sh

--- GalaxyServer/install_galaxy_tools.sh
+++ GalaxyServer/install_galaxy_tools.sh
@@ -2,12 +2,13 @@
 echo "please enter your admin API key: "
 read API
 /root/galaxy/database/dependencies/_conda/bin/conda update conda -y
-/root/galaxy/.venv/bin/pip install ephemeris
+python3 -m venv /root/tools_venv
+/root/tools_venv/bin/pip install ephemeris
 VAR=`grep -c "\- " /root/AnalyseGenome/GalaxyServer/ag_tools.yml`
-printf '\nInstallation of the $VAR galaxy tools may take a while (10/15 min). Keep cool\n'
+printf '\nInstallation of the $VAR galaxy tools may take a while (~16 min). Keep cool\n'
 printf "\nYou'll find the tool install log in ~/tools.log\n\n"
 printf '\nIn the meantime, you can start to browse your Galaxy web interface !\n\n'
-/root/galaxy/.venv/bin/shed-tools install \
+/root/tools_venv/bin/shed-tools install \
                                   -g http://127.0.0.1 \
                                   -a "$API" \
                                   -u admin@galaxy.org \
